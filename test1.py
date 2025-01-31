46474bb18b754a18e57ff4231b0cf62947537e70
Emmanuel-324git config --global user.email emmanuel324@vt.edugit config --global user.name Emmanuel-324 <emmanuel324@vt.edu>
2025-01-31 13:46:30 -0500

    1-31-25

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 # Load the CSV file
-input_file = "/home/emmanuel324/projects/belson324/Crystal_Plasticity/1-20-25/VS/Test1/Two_phase/Data/Two_phase_out.csv"
+input_file = "/home/emmanuel324/projects/belson324/Crystal_Plasticity/1-15-25/Test6a/Data/restartkksmod_work_a_out.csv"
 data = pd.read_csv(input_file)
 df = pd.read_csv(input_file)
 # Ensure 'time' column 
